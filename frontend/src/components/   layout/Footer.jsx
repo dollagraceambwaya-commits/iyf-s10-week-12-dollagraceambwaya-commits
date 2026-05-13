c60dfff1406565a17b0ce826e661c8865787eaec
Lucy Wachu <lucywachu77@gmail.com>
2026-05-13 15:20:29 +0300

    Fix global CSS merge conflict

--- frontend/src/components/   layout/Footer.jsx
+++
@@ -1,9 +0,0 @@
-import '../../styles/Footer.css'
-
-export default function Footer() {
-  return (
-    <footer className="footer">
-      <p>© 2026 Orphanage Finance System | Built by Team 5 - UI by Julie</p>
-    </footer>
-  )
-}
